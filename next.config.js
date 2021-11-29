e13534f68076bcfdc4b9b9f083f76ee6e4cfd154
nmimran99 <nmimran99@gmail.com>
2021-11-29 18:42:59 -0500

    URI changes

--- next.config.js
+++ next.config.js
@@ -1,3 +1,7 @@
-module.exports = {
-  reactStrictMode: true,
-}
+export default {
+	reactStrictMode: true,
+	env: {
+		MONGODB_URI:
+			"mongodb+srv://nivmimran:fUuCBnbi7B2AQDR2@cluster0.gw9wm.mongodb.net/auxroad-landing?retryWrites=true&w=majority",
+	},
+};
