a63eb3e1f8d92e1f04025ea12d6acc43f55c226f
Niv Mimran <41971075+nmimran99@users.noreply.github.com>
2023-01-09 12:06:22 -0500

    Delete next.config.js

--- next.config.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-	reactStrictMode: true,
-	env: {
-		MONGODB_URI:
-			"mongodb+srv://nivmimran:fUuCBnbi7B2AQDR2@cluster0.gw9wm.mongodb.net/auxroad-landing?retryWrites=true&w=majority",
-	},
-};
